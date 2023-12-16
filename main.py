038d46a563f11fc2e7018b915ed061f6b11d7676
Inon Peled <inon.peled@pendo.io>
2023-12-16 20:22:12 +0200

    WIP

--- main.py
+++ main.py
@@ -1,68 +1,8 @@
-"""
-Solver for the New York Times' Letter Boxed Daily Puzzle Game:
-https://www.nytimes.com/puzzles/letter-boxed
+from solve import solve
 
-
-List of English words was extracted from
-https://dumps.wikimedia.org/enwiktionary/20231201/enwiktionary-20231201-pages-articles-multistream-index.txt.bz2
-via
-```cat ./enwiktionary-20231201-pages-articles-multistream-index.txt |
-    cut -f3 -d: |
-    grep -E '^[a-z]*$' >
-    ./only_small_caps.csv
-```
-"""
-
-LETTERBOX = 'WCMHKSOBETAI'
-
-
-def _load_words():
-    """Load words from a file."""
-    with open('only_small_caps.csv') as f:
-        return {line.strip() for line in f.readlines()}
-
-
-def _narrow_down(words, letters):
-    """Narrow down the list of words based on the letters."""
-    letters_set = set(letters.lower())
-    narrowed = {word for word in words if not (set(word) - letters_set)}
-    return narrowed
-
-
-def _is_eligible(word, edge_indexes):
-    word_as_edge_indexes = [edge_indexes[t] for t in word]
-    return all(
-        word_as_edge_indexes[i] != word_as_edge_indexes[i + 1]
-        for i in range(len(word) - 1)
-    )
-
-
-def _eligible_words(narrowed_down_words, letters):
-    edge_indexes = {
-        t: (letters.lower().index(t) // 4)
-        for t in letters.lower()
-    }
-    eligible_words = {
-        w for w in narrowed_down_words
-        if _is_eligible(w, edge_indexes)
-    }
-    return eligible_words
-
-
-def _eligible_pairs(eligible_words, letters):
-    letters_set = set(letters.lower())
-    eligible_pairs = {
-        (w1, w2)
-        for w1 in eligible_words
-        for w2 in eligible_words
-        if (w1[-1] == w2[0]) and (set(w1 + w2) == letters_set)
-    }
-    return eligible_pairs
-
+LETTERBOX = 'trapkhnoeibw'
 
 if __name__ == '__main__':
-    all_words = _load_words()
-    narrowed = _narrow_down(all_words, LETTERBOX)
-    eligible = _eligible_words(narrowed, LETTERBOX)
-    eligible_pairs = _eligible_pairs(eligible, LETTERBOX)
-    print(eligible_pairs)
+    solutions = solve(LETTERBOX)
+    for sol in solutions:
+        print(f'\n{sol}')
---
+++ solve.py
@@ -0,0 +1,88 @@
+"""
+Solver for the New York Times' Letter Boxed Daily Puzzle Game:
+https://www.nytimes.com/puzzles/letter-boxed
+
+
+List of English words was extracted from
+https://dumps.wikimedia.org/enwiktionary/20231201/enwiktionary-20231201-pages-articles-multistream-index.txt.bz2
+via
+```cat ./enwiktionary-20231201-pages-articles-multistream-index.txt |
+    cut -f3 -d: |
+    grep -E '^[a-z]*$' >
+    ./only_small_caps.csv
+```
+"""
+
+# NOTE: you'll also need to install external libraries for the following import to work.
+# In macOS, these libraries can be installed via `brew install enchant`
+import enchant
+
+
+def _load_words():
+    """Load words from a file."""
+    with open('only_small_caps.csv') as f:
+        return {line.strip() for line in f.readlines()}
+
+
+def _narrow_down(words, letters):
+    """Narrow down to English words that consist only of the given letters, disregarding box edges."""
+    letters_set = set(letters.lower())
+    narrowed = {word for word in words if not (set(word) - letters_set)}
+
+    english_dict = enchant.Dict('en_US')
+    narrowed_english = {word for word in narrowed if english_dict.check(word)}
+
+    return narrowed_english
+
+
+def _is_eligible(word, edge_indexes):
+    word_as_edge_indexes = [edge_indexes[t] for t in word]
+    return all(
+        word_as_edge_indexes[i] != word_as_edge_indexes[i + 1]
+        for i in range(len(word) - 1)
+    )
+
+
+def _eligible_words(narrowed_down_words, letters):
+    edge_indexes = {
+        t: (letters.lower().index(t) // 3)
+        for t in letters.lower()
+    }
+    eligible_words = {
+        w for w in narrowed_down_words
+        if _is_eligible(w, edge_indexes)
+    }
+    return eligible_words
+
+
+def _eligible_pairs(eligible_words, letters):
+    letters_set = set(letters.lower())
+    eligible_pairs = {
+        (w1, w2)
+        for w1 in eligible_words
+        for w2 in eligible_words
+        if (w1[-1] == w2[0]) and (set(w1 + w2) == letters_set)
+    }
+    return eligible_pairs
+
+
+def _find_single_word_solutions(eligible_words, letters):
+    letters_set = set(letters.lower())
+    single_word_solutions = {
+        w for w in eligible_words
+        if set(w) == letters_set
+    }
+    return single_word_solutions
+
+
+def solve(letters):
+    assert len(letters) == 12 and len(set(letters)) == 12 and letters.isalpha()
+
+    all_words = _load_words()
+    narrowed = _narrow_down(all_words, letters)
+    eligible = _eligible_words(narrowed, letters)
+
+    single_word_solutions = _find_single_word_solutions(eligible, letters)
+    two_word_solutions = _eligible_pairs(eligible, letters)
+
+    return single_word_solutions | two_word_solutions
