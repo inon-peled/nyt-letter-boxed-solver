6c4456263daa2ebb7d6207555145b13309566cf3
Inon Peled <inon.peled@pendo.io>
2023-12-16 22:01:55 +0200

    Test single word solution

--- test.py
+++ test.py
@@ -14,3 +14,14 @@ def test_solve_2():
     assert solve(english_words_csv_path='english_words.csv', letters='traeonwbihkp') == {
         ('pawnbroker', 'rethink')
     }
+
+
+def test_solve_3():
+    word = 'stenographic'
+    letter_boxed = ''
+    for i in range(3):
+        letter_boxed += word[i] + word[i + 3] + word[i + 6] + word[i + 9]
+
+    solutions = solve(english_words_csv_path='english_words.csv', letters=letter_boxed)
+
+    assert word in solutions
