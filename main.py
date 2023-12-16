aa2d5c208621e273d8c80870e45aec37d8d0060b
Inon Peled <inon.peled@pendo.io>
2023-12-16 21:31:38 +0200

    note

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from preprocess import preprocess
 from solve import solve
 
+# ==== Assign the letters of the letterbox to the following constant.
+# * Use exactly 12 letters, no spaces, no punctuation.
+# * First the 3 letters of one letterbox edge (say, top edge), then the 3 letters of another edge, etc.
+# * Letter case does not matter.
 LETTERBOX = 'wcmhksobetai'
 
 
