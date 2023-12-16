5e94b350ea9b77785333955190aa4edc470c90f4
Inon Peled <inon.peled@pendo.io>
2023-12-16 21:40:33 +0200

    Example

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 1. Assign the letters of the letterbox to LETTERBOX below.
     * Use exactly 12 letters, no spaces, no punctuation.
-    * First the 3 letters of one letterbox edge (say, top edge), then the 3 letters of another edge, etc.
-    * Letter case does not matter.
+    * First the 3 letters of one letterbox edge (say, the top edge), then the 3 letters of another edge, etc.
+    * Letter case and the order of letters of the same edge do not matter.
 2. Run this script.
 """
+# For example, the following letters correspond to the letterbox in `example.png`.
 LETTERBOX = 'wcmhksobetai'
 
 from preprocess import preprocess
